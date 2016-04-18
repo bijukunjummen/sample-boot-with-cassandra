c319815e04159b3644d6d224b31d0cfd6a5fd256
Biju Kunjummen <biju.kunjummen@gmail.com>
2016-04-17 23:14:20 -0700

    Spring boot with Cassandra sample

---
+++ src/main/java/cass/domain/Hotel.java
@@ -0,0 +1,83 @@
+package cass.domain;
+
+import com.google.common.base.MoreObjects;
+import org.springframework.data.cassandra.mapping.PrimaryKey;
+import org.springframework.data.cassandra.mapping.Table;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Table("hotels")
+public class Hotel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @PrimaryKey
+    private UUID id;
+
+    private String name;
+
+    private String address;
+
+    private String zip;
+
+    private Integer version;
+
+    public Hotel() {
+    }
+
+    public Hotel(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getZip() {
+        return this.zip;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("address", address)
+                .add("zip", zip)
+                .add("version", version)
+                .toString();
+    }
+}
---
+++ src/main/java/cass/repository/HotelRepository.java
@@ -0,0 +1,8 @@
+package cass.repository;
+
+import cass.domain.Hotel;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface HotelRepository extends CrudRepository<Hotel, UUID>{}
