4756684307cd5e6be789efb2a0e457d1f367ea13
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2016-10-31 13:31:42 -0700

    Fixed readme

--- src/main/java/cass/domain/HotelByLetterKey.java
+++ src/main/java/cass/domain/HotelByLetterKey.java
@@ -18,7 +18,7 @@ public class HotelByLetterKey implements Serializable {
     @PrimaryKeyColumn(name="hotel_name", ordinal = 0, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
     private String name;
 
-    @Column("hotel_id")
+    @PrimaryKeyColumn(name="hotel_id", ordinal = 1, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
     private UUID hotelId;
 
     public UUID getHotelId() {
